f36f7b573ab785c7a9faf9c138792a8a2e3bdd87
ACMANI-LOVE <atomos.fee24@gmail.com>
2025-12-06 16:29:02 +0900

    UPload

--- usx/next.config.ts
+++ usx/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactStrictMode: true,
-  basePath: "/myGame", // GitHub Pages でリポジトリ名付きURLなら設定
   reactCompiler: true,
 };
 
