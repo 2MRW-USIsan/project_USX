fdbbe0536b401187b9307b6874d38cb207213b65
ACMANI-LOVE <atomos.fee24@gmail.com>
2025-12-06 16:05:19 +0900

    GitHubPages Settings

--- usx/next.config.ts
+++ usx/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/<repository-name>", // GitHub Pages でリポジトリ名付きURLなら設定
   reactCompiler: true,
 };
 
